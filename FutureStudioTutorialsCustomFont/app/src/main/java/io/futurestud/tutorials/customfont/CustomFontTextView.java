9908467e0d7d8a43e7c512f23ca97d2acfbbc1b0
Norman Peitek <norman.peitek@gmail.com>
2015-03-08 17:35:49 +0100

    improved custom font textview class to understand textStyle xml property

--- FutureStudioTutorialsCustomFont/app/src/main/java/io/futurestud/tutorials/customfont/CustomFontTextView.java
+++ FutureStudioTutorialsCustomFont/app/src/main/java/io/futurestud/tutorials/customfont/CustomFontTextView.java
@@ -10,26 +10,50 @@ import android.widget.TextView;
  */
 public class CustomFontTextView extends TextView {
 
+    public static final String ANDROID_SCHEMA = "http://schemas.android.com/apk/res/android";
+
     public CustomFontTextView(Context context) {
         super(context);
 
-        applyCustomFont(context);
+        applyCustomFont(context, null);
     }
 
     public CustomFontTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        applyCustomFont(context);
+        applyCustomFont(context, attrs);
     }
 
     public CustomFontTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        applyCustomFont(context);
+        applyCustomFont(context, attrs);
     }
 
-    private void applyCustomFont(Context context) {
-        Typeface customFont = FontCache.getTypeface("SourceSansPro-Regular.ttf", context);
+    private void applyCustomFont(Context context, AttributeSet attrs) {
+        int textStyle = attrs.getAttributeIntValue(ANDROID_SCHEMA, "textStyle", 0);
+
+        Typeface customFont;
+
+        /*
+         information about the TextView textStyle:
+         http://developer.android.com/reference/android/R.styleable.html#TextView_textStyle
+          */
+        switch (textStyle) {
+            case 1: // bold
+                customFont = FontCache.getTypeface("SourceSansPro-Bold.ttf", context);
+                break;
+
+            case 2: // italic
+                customFont = FontCache.getTypeface("SourceSansPro-Italic.ttf", context);
+                break;
+
+            case 0: // regular
+            default:
+                customFont = FontCache.getTypeface("SourceSansPro-Regular.ttf", context);
+
+        }
+
         setTypeface(customFont);
     }
 }
