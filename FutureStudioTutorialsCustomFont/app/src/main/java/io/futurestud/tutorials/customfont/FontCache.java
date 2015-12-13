55e9dcc60a03fabb859bffc1bd0edda4aa4c620d
Norman Peitek <norman.peitek@gmail.com>
2015-12-13 17:24:13 +0100

    code reformat

--- FutureStudioTutorialsCustomFont/app/src/main/java/io/futurestud/tutorials/customfont/FontCache.java
+++ FutureStudioTutorialsCustomFont/app/src/main/java/io/futurestud/tutorials/customfont/FontCache.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 /**
  * Created by norman on 3/8/15.
- *
+ * <p/>
  * Code taken from britzl on StackOverflow (slightly modified):
  * http://stackoverflow.com/questions/16901930/memory-leaks-with-custom-font-for-set-custom-font/16902532#16902532
  */
--- FutureStudioTutorialsCustomFont/app/src/main/java/io/futurestud/tutorials/customfont/MainActivity.java
+++ FutureStudioTutorialsCustomFont/app/src/main/java/io/futurestud/tutorials/customfont/MainActivity.java
@@ -1,7 +1,7 @@
 package io.futurestud.tutorials.customfont;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,15 +10,15 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.menu_main, menu );
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -34,6 +34,6 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected( item );
+        return super.onOptionsItemSelected(item);
     }
 }
